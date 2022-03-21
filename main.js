a12321d3a9b3eec2a2b17b53506f65aa50e0b6b4
rusinner <rusinner88@gmail.com>
2022-03-21 16:47:54 +0200

    transitions-ok-forms-need-last-details

--- main.js
+++ main.js
@@ -3,15 +3,20 @@ const sign_up_btn = document.querySelector("#sign-up-button");
 const container = document.querySelector(".container");
 const left_panel = document.querySelector(".left-panel");
 const right_panel = document.querySelector("#right-panel");
+const signup_form = document.querySelector(".signup-signup");
 
 
 sign_up_btn.addEventListener("click", () =>{
     container.classList.add("sign-up-mode");
     left_panel.classList.add("move-left-image");
     right_panel.classList.add("move-right-image");
+    signup_form.classList.add("move-signup");
 });
 
 sign_in_btn.addEventListener("click", () =>{
     container.classList.remove("sign-up-mode");
+    left_panel.classList.remove("move-left-image");
+    right_panel.classList.remove("move-right-image");
+    signup_form.classList.remove("move-signup");
 
 });

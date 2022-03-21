a4c46ef2f5ab8669e9c71da09045d98911eeb075
rusinner <rusinner88@gmail.com>
2022-03-21 15:35:56 +0200

    signup-signup-need-fix

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ const sign_in_btn = document.querySelector("#sign-in-button");
 const sign_up_btn = document.querySelector("#sign-up-button");
 const container = document.querySelector(".container");
 const left_panel = document.querySelector(".left-panel");
+const right_panel = document.querySelector("#right-panel");
+
 
 sign_up_btn.addEventListener("click", () =>{
     container.classList.add("sign-up-mode");
     left_panel.classList.add("move-left-image");
+    right_panel.classList.add("move-right-image");
 });
 
 sign_in_btn.addEventListener("click", () =>{
     container.classList.remove("sign-up-mode");
+
 });
